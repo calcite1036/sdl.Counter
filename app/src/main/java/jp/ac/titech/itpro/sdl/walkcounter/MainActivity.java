ad65ed103c6c8cce7e4e225dc2a1d64f657e4553
calcite1036 <hakkyopirotto@gmail.com>
2021-06-18 13:22:08 +0900

    initial commit

---
+++ app/src/main/java/jp/ac/titech/itpro/sdl/walkcounter/MainActivity.java
@@ -0,0 +1,72 @@
+package jp.ac.titech.itpro.sdl.walkcounter;
+
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+    private final static String TAG = MainActivity.class.getSimpleName();
+
+    private RotationView rotationView;
+    private SensorManager manager;
+    private Sensor gyroscope;
+
+    private long prevTimestamp=0;
+    private float omega=0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d(TAG, "onCreate");
+
+        rotationView = findViewById(R.id.rotation_view);
+
+        manager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        if (manager == null) {
+            Toast.makeText(this, R.string.toast_no_sensor_manager, Toast.LENGTH_LONG).show();
+            finish();
+            return;
+        }
+        gyroscope = manager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+        if (gyroscope == null) {
+            Toast.makeText(this, R.string.toast_no_gyroscope, Toast.LENGTH_LONG).show();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume");
+        manager.registerListener(this, gyroscope, SensorManager.SENSOR_DELAY_FASTEST);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause");
+        manager.unregisterListener(this);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        float omegaZ = event.values[2];  // z-axis angular velocity (rad/sec)
+        long ts = event.timestamp;
+        if (prevTimestamp != 0) {
+            omega = omega + omegaZ * (ts - prevTimestamp) / 1000000000;
+        }
+        rotationView.setDirection(omega);
+        prevTimestamp = ts;
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        Log.d(TAG, "onAccuracyChanged: accuracy=" + accuracy);
+    }
+}
